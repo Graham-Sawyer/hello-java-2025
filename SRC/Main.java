3036e4da4add5906b04164a4f54b4046cdd60017
Graham-Sawyer <gssawyeriastate@gmail.com>
2025-12-02 18:13:43 +0000

    First commit

---
+++ SRC/Main.java
@@ -0,0 +1,7 @@
+class Main {                    // remove "public"
+    public static void main(String[] args) {
+        System.out.println("Hello from Codespaces 🥳 I'm back!");
+        System.out.println("Meche turned developer 2025");
+    }
+}
+
